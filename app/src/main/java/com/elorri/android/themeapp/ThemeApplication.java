60305d2470a9bd4a14113fc94872660eb93bf4cc
Elorri <etchemendy.elorri@gmail.com>
2016-11-04 08:14:42 +0100

    nit

---
+++ app/src/main/java/com/elorri/android/themeapp/ThemeApplication.java
@@ -0,0 +1,8 @@
+package com.elorri.android.themeapp;
+
+import android.app.Application;
+
+public class ThemeApplication extends Application {
+    // App level variable to retain selected spinner value
+    public static int currentPosition;
+}
